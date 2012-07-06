1e727968ab920c4c3660e0c07d23a4afdc796641
Wendy <wendy@localhost.localdomain>
2012-07-06 00:04:09 -0700

    noncommittal msg

--- ex13-shuntingyard.py
+++ ex13-shuntingyard.py
@@ -13,45 +13,63 @@ def consume(token_list):
     return token_list.pop(0)
 
 def build_parse_tree(token_list):
-operation_dictionary = {"*":3, "/":3, "+":2, "-":2}
+	queue = []
+	stack = []
 
-#if token is ")", 
-    #Until token at top of stack is "(" pop operators off the stack combining w/last 2 elements
-    #to create binary tree node and putting on stack
+	oper_dict = {"*":3, "/":3, "+":2, "-":2}
 
-    while len(token_list) > 0:
-        token = consume(token_list)
-        output_queue = []
-        operator_stack = []
-        if is_num(token):
-            output_queue.append(token)
-        elif token in "+-*/":
-            if len(operator_stack) == 0:
-            root = BinaryTreeNode(token)
-            root.left = output_queue.pop(-2)
-            root.right = output_queue.pop(-1)
-            output_queue.append(root)
-            elif operation_dictionary(token) > operation_dictionary(operator_stack[-1]):
-                output_queue.append(token)
-            elif operation_dictionary(token) <= operation_dictionary(operator_stack[-1]):    
-                output_queue.append(operator_stack[-1])
-                operator_stack.append(token)
+	while len(token_list) > 0:
+		token = consume(token_list)
+		if is_num(token):
+			queue.append(int(token))
+		elif token in ['*', '/', '-', '+']:
+			o1 = token
+			while len(stack) > 0 and stack[-1] in ['*', '/', '-', '+']: 
+				o2 = stack[-1]
+				if oper_dict[o1] <= oper_dict[o2]:
+					o2 = stack.pop()
+					right = queue.pop() # take last 2 elements from queue
+					left = queue.pop() 
+					root = BinaryTreeNode(o2) # build binary tree
+					if is_num(left):
+						left = BinaryTreeNode(left)
+					if is_num(right):
+						right = BinaryTreeNode(right)
+					root.left = left # set left and right children
+					root.right = right 
+					queue.append(root)
+			stack.append(o1)
+		elif token == "(":
+			stack.append(token)
+		elif token == ")":
+			while stack[-1] != "(":
+				n = stack.pop()
+				right = queue.pop()
+				left = queue.pop()
+				root = BinaryTreeNode(n)
+				if is_num(left):
+					left = BinaryTreeNode(left)
+				if is_num(right):
+					right = BinaryTreeNode(right)
+				root.left = left
+				root.right = right 
+				queue.append(root)
+			stack.pop()
 
+	while len(stack) > 0:
+		n = stack.pop()
+		right = queue.pop()
+		left = queue.pop()
+		root = BinaryTreeNode(n)
+		if is_num(left):
+			left = BinaryTreeNode(left)
+		if is_num(right):
+			right = BinaryTreeNode(right)
+		root.left = left
+		root.right = right
+		queue.append(root)
 
-        elif token == "(":
-            operator_stack.push(token)
-        #>>>>>>>>elif token == ")":
-        left = BinaryTreedNode(output_queue[0])
-        right = BinaryTreedNode(output_queue[1])
-
-    """Takes in a token list and produces a parse tree according to the rules in the README"""
-    # IMPLEMENT SHUNTING YARD ALGORITHM HERE
-    root = BinaryTreeNode("+")
-    left = BinaryTreeNode(3)
-    right = BinaryTreeNode(2)
-    root.left = left
-    root.right = right
-    return root
+	return queue[-1]
 
 def is_num(string):
     try:
@@ -76,7 +94,7 @@ def evaluate_tree(node):
 
 def main():
     sample_string = "(((3 + 2) * (4 - 1) / 3 + 7) * (1 + (-5 + 6)))"
-    sample_string = "(3 + (2 + 5))"
+    #sample_string = "(3 + (2 + 5))"
     tokens = tokenize(sample_string)
     tree = build_parse_tree(tokens)
     print evaluate_tree(tree)
